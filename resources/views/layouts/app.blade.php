1782a9f2daf19d139ff534687fe595e28d736411
Uki7991 <tilek.kubanov@gmail.com>
2018-08-09 13:42:55 +0600

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Forum|Lobster&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed:300,300i,400,400i,500,500i,600,600i,700,700i"
+          rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
@@ -18,85 +19,92 @@
 
 </head>
 <body>
-    <div id="app" class="mb-5">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <nav class="navbar shadow navbar-expand-md navbar-light">
+        <div class="container">
+            <a class="navbar-brand p-0" href="/"><img src="images/2.1.png" style="width:100px; height:auto;" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Вход') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Вход') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="row justify-content-center text-center" >
-                <div class="col-3 p-2"><img src="images/2.png" style="width:120px; height:100px;" alt=""></div>
-                <div class="col-5 p-5">
-                    <form class="example" action="/">
-                        <input class="text-center" type="text" placeholder="Search.." name="search">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
-                <div class="col-2 p-5">Плюшка 1</div>
-                <div class="col-2 p-5">Плюшка 2</div>
+                </ul>
             </div>
         </div>
-        <nav class="navbar-2 navbar-expand-md navbar-light navbar-laravel " style="border-top: solid 0.1px gray; border-bottom: solid 0.1px gray">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                    <ul class="navbar-menu" style="margin-left: 200px;">
-                        <li><a style="color:black!important;" href="">Алкоголь</a></li>
-                        <li><a style="color:black!important;" href="">Сигареты</a></li>
-                        <li><a style="color:black!important;" href="">Драгоценности</a></li>
-                        <li><a style="color:black!important;" href="">Подарки</a></li>
-                        <li><a style="color:black!important;" href="">Аксессуары</a></li>
-                        <li><a style="color:black!important;" href="">Прочее</a></li>
-                    </ul>
+    </nav>
+    <div class="container">
+        <div class="row justify-content-center text-center">
+            <div class="col-3 p-2"><img src="images/2.1.png" style="width:100px; height:auto;" alt=""></div>
+            <div class="col-5 p-5">
+                <form class="example" action="/">
+                    <input class="text-center" type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
             </div>
-        </nav>
+            <div class="col-2 p-5">Плюшка 1</div>
+            <div class="col-2 p-5">Плюшка 2</div>
+        </div>
     </div>
-    @yield('content')
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/classie.js') }}" defer></script>
+
+    <div class="container border-top border-bottom py-2">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Алкоголь</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Сигареты</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Драгоценности</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Подарки</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Аксессуары</a>
+            </li>
+        </ul>
+    </div>
+
+@yield('content')
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/classie.js') }}" defer></script>
 
 </body>
 </html>
