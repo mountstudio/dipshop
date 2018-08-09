5b955948e10cfb15ae0b03a756268a0400800871
Uki7991 <tilek.kubanov@gmail.com>
2018-08-09 12:42:48 +0600

    product-card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,198 +1,147 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container p-5 bg-white">
-            <div class="row justify-content-center">
-            <div class="col-4" style="background-image: url('images/1.png'); width:100%; height: 370px; background-size: cover"></div>
-            <div class="col-6" style="background-image: url('images/4.png'); width:100%; height: 370px; background-size: cover"></div>
-            </div>
+    <div class="container p-5 bg-white">
+        <div class="row justify-content-center">
+            <div class="col-4"
+                 style="background-image: url('images/1.png'); width:100%; height: 370px; background-size: cover"></div>
+            <div class="col-6"
+                 style="background-image: url('images/4.png'); width:100%; height: 370px; background-size: cover"></div>
         </div>
+    </div>
 
     <div class="container mb-5">
         <div class="row">
-            <div class="col-3 pl-2 pr-2 mb-2">
-        <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-            <div class="article" style="height: 20px;">
-                <span class="float-left">Виски</span>
-                <span class="float-right">#15864</span>
-            </div>
-            <div class="description">
-                <p>Jack Daniels 0.5</p>
-            </div>
-            <div class="prod-img mb-3" style="background-image: url('images/1.png')">
-            </div>
 
-            <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                <span class="text-left main-font">Цена</span>
-                <span class="float-right main-font">100$</span><br>
-                <span class="text-left main-font">Кол-во</span>
-                <span class="float-right main-font">1</span>
 
-            </div>
-            <div><button style="height: 40px; width: 100%;">В корзину</button></div>
-        </div>
-        </div>
+            {{--<div class="col-3 mb-4">--}}
+                {{--<div class="shadow card rounded bg-white border border-light p-3">--}}
+                    {{--<div class="article" style="height: 20px;">--}}
+                        {{--<span class="float-left">Виски</span>--}}
+                        {{--<span class="float-right">#15864</span>--}}
+                    {{--</div>--}}
+                    {{--<div class="description">--}}
+                        {{--<p>Jack Daniels 0.5</p>--}}
+                    {{--</div>--}}
+                    {{--<div class="prod-img mb-3" style="background-image: url('images/1.png')">--}}
+                    {{--</div>--}}
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
+                    {{--<div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">--}}
+                        {{--<span class="text-left main-font">Цена</span>--}}
+                        {{--<span class="float-right main-font">100$</span><br>--}}
+                        {{--<span class="text-left main-font">Кол-во</span>--}}
+                        {{--<span class="float-right main-font">1</span>--}}
+
+                    {{--</div>--}}
+                    {{--<div>--}}
+                        {{--<a class="btn btn-outline-success">В корзину</a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+
+            <div class="col-3 mb-4">
+                <div class="card px-4 pt-3 border">
+                    <img class="card-img-top" src="/images/0_0_orig.png" alt="Card image cap">
+                    <div class="card-body d-flex px-0 pb-0">
+                        <div class="text-capitalize mr-auto font-weight-bold">whiskey</div>
+                        <div class="ml-auto text-muted">#21243</div>
                     </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
+                    <div class="card-body px-0 pb-0 d-flex">
+                        <div class="card-title mr-auto">Card title</div>
+                        <div class="card-title ml-auto">Card title</div>
                     </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
+                    <div class="card-body px-0  text-center">
+                        <a href="#" class="btn btn-outline-success shadow-lg">В корзину</a>
                     </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
                 </div>
             </div>
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
+            <div class="col-3 mb-4">
+                <div class="card px-4 pt-3 border">
+                    <img class="card-img-top" src="/images/0_0_orig.png" alt="Card image cap">
+                    <div class="card-body d-flex px-0 pb-0">
+                        <div class="text-capitalize mr-auto font-weight-bold">whiskey</div>
+                        <div class="ml-auto text-muted">#21243</div>
                     </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
+                    <div class="card-body px-0 pb-0 d-flex">
+                        <div class="card-title mr-auto">Card title</div>
+                        <div class="card-title ml-auto">Card title</div>
                     </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
+                    <div class="card-body px-0  text-center">
+                        <a href="#" class="btn btn-outline-success shadow-lg">В корзину</a>
                     </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
                 </div>
             </div>
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
+            <div class="col-3 mb-4">
+                <div class="card px-4 pt-3 border">
+                    <img class="card-img-top" src="/images/0_0_orig.png" alt="Card image cap">
+                    <div class="card-body d-flex px-0 pb-0">
+                        <div class="text-capitalize mr-auto font-weight-bold">whiskey</div>
+                        <div class="ml-auto text-muted">#21243</div>
                     </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
+                    <div class="card-body px-0 pb-0 d-flex">
+                        <div class="card-title mr-auto">Card title</div>
+                        <div class="card-title ml-auto">Card title</div>
                     </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
+                    <div class="card-body px-0  text-center">
+                        <a href="#" class="btn btn-outline-success shadow-lg">В корзину</a>
                     </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
                 </div>
             </div>
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
+            <div class="col-3 mb-4">
+                <div class="card px-4 pt-3 border">
+                    <img class="card-img-top" src="/images/0_0_orig.png" alt="Card image cap">
+                    <div class="card-body d-flex px-0 pb-0">
+                        <div class="text-capitalize mr-auto font-weight-bold">whiskey</div>
+                        <div class="ml-auto text-muted">#21243</div>
                     </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
+                    <div class="card-body px-0 pb-0 d-flex">
+                        <div class="card-title mr-auto">Card title</div>
+                        <div class="card-title ml-auto">Card title</div>
                     </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
+                    <div class="card-body px-0  text-center">
+                        <a href="#" class="btn btn-outline-success shadow-lg">В корзину</a>
                     </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
                 </div>
             </div>
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
+            <div class="col-3 mb-4">
+                <div class="card px-4 pt-3 border">
+                    <img class="card-img-top" src="/images/0_0_orig.png" alt="Card image cap">
+                    <div class="card-body d-flex px-0 pb-0">
+                        <div class="text-capitalize mr-auto font-weight-bold">whiskey</div>
+                        <div class="ml-auto text-muted">#21243</div>
                     </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
+                    <div class="card-body px-0 pb-0 d-flex">
+                        <div class="card-title mr-auto">Card title</div>
+                        <div class="card-title ml-auto">Card title</div>
                     </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
+                    <div class="card-body px-0  text-center">
+                        <a href="#" class="btn btn-outline-success shadow-lg">В корзину</a>
                     </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
                 </div>
             </div>
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
+            <div class="col-3 mb-4">
+                <div class="card px-4 pt-3 border">
+                    <img class="card-img-top" src="/images/0_0_orig.png" alt="Card image cap">
+                    <div class="card-body d-flex px-0 pb-0">
+                        <div class="text-capitalize mr-auto font-weight-bold">whiskey</div>
+                        <div class="ml-auto text-muted">#21243</div>
                     </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
+                    <div class="card-body px-0 pb-0 d-flex">
+                        <div class="card-title mr-auto">Card title</div>
+                        <div class="card-title ml-auto">Card title</div>
                     </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
+                    <div class="card-body px-0  text-center">
+                        <a href="#" class="btn btn-outline-success shadow-lg">В корзину</a>
                     </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
                 </div>
             </div>
 
-            <div class="col-3 pl-2 pr-2 mb-2">
-                <div class="product-card rounded bg-white border border-light p-3" style="width:100%; height: 455px;">
-                    <div class="article" style="height: 20px;">
-                        <span class="float-left">Виски</span>
-                        <span class="float-right">#15864</span>
-                    </div>
-                    <div class="description">
-                        <p>Jack Daniels 0.5</p>
-                    </div>
-                    <div class="prod-img mb-3" style="background-image: url('images/1.png')">
-                    </div>
-
-                    <div class="price p-2" style="border-top: solid 1.5px #e1e1e1;">
-                        <span class="text-left main-font">Цена</span>
-                        <span class="float-right main-font">100$</span><br>
-                        <span class="text-left main-font">Кол-во</span>
-                        <span class="float-right main-font">1</span>
-
-                    </div>
-                    <div><button style="height: 40px; width: 100%;">В корзину</button></div>
-                </div>
-            </div>
         </div>
     </div>
 @endsection
