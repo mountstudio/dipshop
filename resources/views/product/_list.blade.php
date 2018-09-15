a361da2867fd8c253db755943c12df158d8ad493
uki7991 <tilek.kubanov@gmail.com>
2018-09-15 13:26:00 +0600

    style

--- resources/views/product/_list.blade.php
+++ resources/views/product/_list.blade.php
@@ -2,7 +2,7 @@
     @foreach($products as $product)
         <div class="col-3 mb-4">
             <div class="card hover-shadow transition-1000 px-4 pt-3 border-0">
-                <img class="card-img-top" style="max-height: 300px; min-height: 300px; width: auto;" src="{{ asset('uploads/'.$product->image) }}" alt="Card image cap">
+                <img class="card-img-top" src="{{ asset('uploads/'.$product->image) }}" alt="Card image cap">
                 <div class="card-body d-flex px-0 pb-0">
                     <div class="text-capitalize mr-auto font-weight-bold">{{ $product->category->name }}</div>
                     <div class="ml-auto text-muted">#21243</div>
