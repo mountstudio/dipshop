683b31600fa4aaae1807cc391be09b5b0a2fc36a
uki7991 <tilek.kubanov@gmail.com>
2018-09-15 13:54:56 +0600

    fix card

--- resources/views/product/_list.blade.php
+++ resources/views/product/_list.blade.php
@@ -1,14 +1,14 @@
 <div class="row">
     @foreach($products as $product)
         <div class="col-3 mb-4">
-            <div class="card hover-shadow transition-1000 px-4 pt-3 border-0">
+            <div class="card hover-shadow border-bottom border-top-0 border-left-0 border-right-0 transition-1000 px-4 pt-3 border-0">
                 <img class="card-img-top" src="{{ asset('uploads/'.$product->image) }}" alt="Card image cap">
                 <div class="card-body d-flex px-0 pb-0">
                     <div class="text-capitalize mr-auto font-weight-bold">{{ $product->category->name }}</div>
                     <div class="ml-auto text-muted">#21243</div>
                 </div>
                 <div class="card-body px-0 pb-0 pt-1 d-flex">
-                    <div class="card-title mr-auto">{{ $product->name }}</div>
+                    <div class="card-title mr-auto" style="min-height: 46px;">{{ $product->name }}</div>
                     <div class="card-title ml-auto font-weight-bold">${{ $product->price }}</div>
                 </div>
                 <div class="card-body px-0  text-center">
