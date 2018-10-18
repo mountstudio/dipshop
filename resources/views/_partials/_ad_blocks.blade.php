805c9121e96f66cf893eda2bc700d0ec967972a0
uki7991 <tilek.kubanov@gmail.com>
2018-10-19 02:36:08 +0600

    images

--- resources/views/_partials/_ad_blocks.blade.php
+++ resources/views/_partials/_ad_blocks.blade.php
@@ -4,18 +4,18 @@
         <div class="col-12">
             <div class="row">
                 <div class="col-4">
-                    <a href="">
-                        <img src="{{ asset('images/ads/jimmy.jpg') }}" class="img-fluid" alt="">
+                    <a href="{{ route('alcohol') }}">
+                        <img src="{{ asset('images/banners/4.jpg') }}" class="img-fluid" alt="">
                     </a>
                 </div>
                 <div class="col-4">
-                    <a href="">
-                        <img src="{{ asset('images/ads/gucci.jpg') }}" class="img-fluid" alt="">
+                    <a href="{{ route('cigaretes') }}">
+                        <img src="{{ asset('images/banners/1.jpg') }}" class="img-fluid" alt="">
                     </a>
                 </div>
                 <div class="col-4">
-                    <a href="">
-                        <img src="{{ asset('images/ads/armani.jpg') }}" class="img-fluid" alt="">
+                    <a href="{{ route('alcohol') }}">
+                        <img src="{{ asset('images/banners/10.jpg') }}" class="img-fluid" alt="">
                     </a>
                 </div>
             </div>
