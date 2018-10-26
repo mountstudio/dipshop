5f08f03c260f48955f75a314a2b8f6ed15754b91
mackinkenny <Mackinkenny@gmail.com>
2018-10-26 12:50:02 +0600

    user data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('lastname');
+            $table->string('address');
+            $table->integer('phonenumber');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('personalid');
             $table->rememberToken();
             $table->timestamps();
         });
