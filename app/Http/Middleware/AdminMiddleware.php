9c3578c6f206157d2d4ef4f288c7d670a3aeeacc
uki7991 <tilek.kubanov@gmail.com>
2018-10-27 00:30:31 +0600

    fix

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -15,7 +15,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (\Auth::user() && \Auth::user()->admin === 1) {
+        if (\Auth::user() && \Auth::user()->admin == 1) {
             return $next($request);
         }
 
