3d846a897ec7627726bdd6a6573d4f0879f26add
Saitama7 <djkaira7@gmail.com>
2018-11-01 22:11:36 +0600

    Order image src fix

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -27,7 +27,7 @@
                     <td class="p-1">
                         <div class="d-flex justify-content-center align-items-center">
                             <div class="col-auto">
-                                <img class="img-fluid" width="90" height="90" src="http://dipshop/uploads/small/{{ $product['item']->image }}">
+                                <img class="img-fluid" width="90" height="90" src="/uploads/small/{{ $product['item']->image }}">
                             </div>
                         </div>
                     </td>
